226e08bd0a612a59b097d6dd77be80208f57a7d6
alexjeilert <alexjeilert@gmail.com>
2023-03-07 16:38:40 -0600

    Added __init__

--- contrib/capitalone_dataprofiler_expectations/capitalone_dataprofiler_expectations/rule_based_profiler/domain_builder/data_profiler_column_domain_builder.py
+++ contrib/capitalone_dataprofiler_expectations/capitalone_dataprofiler_expectations/rule_based_profiler/domain_builder/data_profiler_column_domain_builder.py
@@ -41,7 +41,40 @@ if TYPE_CHECKING:
     )
     from great_expectations.validator.validator import Validator
 
+
 class DataProfilerColumnDomainBuilder(ColumnDomainBuilder):
+
+    def __init__(
+        self,
+        include_column_names: Optional[Union[str, Optional[List[str]]]] = None,
+        exclude_column_names: Optional[Union[str, Optional[List[str]]]] = None,
+        include_column_name_suffixes: Optional[Union[str,
+                                                     Iterable, List[str]]] = None,
+        exclude_column_name_suffixes: Optional[Union[str,
+                                                     Iterable, List[str]]] = None,
+        semantic_type_filter_module_name: Optional[str] = None,
+        semantic_type_filter_class_name: Optional[str] = None,
+        include_semantic_types: Optional[
+            Union[str, SemanticDomainTypes,
+                  List[Union[str, SemanticDomainTypes]]]
+        ] = None,
+        exclude_semantic_types: Optional[
+            Union[str, SemanticDomainTypes,
+                  List[Union[str, SemanticDomainTypes]]]
+        ] = None,
+        data_context: Optional[AbstractDataContext] = None,
+    ) -> None:
+        super().__init__(
+            include_column_names=include_column_names,
+            exclude_column_names=exclude_column_names,
+            include_column_name_suffixes=include_column_name_suffixes,
+            exclude_column_name_suffixes=exclude_column_name_suffixes,
+            semantic_type_filter_module_name=semantic_type_filter_module_name,
+            semantic_type_filter_class_name=semantic_type_filter_class_name,
+            include_semantic_types=include_semantic_types,
+            exclude_semantic_types=exclude_semantic_types,
+            data_context=data_context,
+        )
 
     def _get_domains(
         self,
@@ -79,7 +112,8 @@ class DataProfilerColumnDomainBuilder(ColumnDomainBuilder):
             column_names=effective_column_names,
             domain_type=self.domain_type,
             # type: ignore[union-attr] # could be None
-            table_column_name_to_inferred_semantic_domain_type_map=self.semantic_type_filter.table_column_name_to_inferred_semantic_domain_type_map,
+            table_column_name_to_inferred_semantic_domain_type_map=None,
+            # self.semantic_type_filter.table_column_name_to_inferred_semantic_domain_type_map,
         )
 
         return domains
@@ -95,19 +129,17 @@ class DataProfilerColumnDomainBuilder(ColumnDomainBuilder):
         This method applies multiple directives to obtain columns to be included as part of returned "Domain" objects.
         """
 
-        
         effective_column_names = list()
 
-        
-
         rule_name_to_data_types = {"numeric_rule": {"int", "float"}, "timestamp_rule": {
-            "datetime"}, "text_rule": {"string"}, "categorical_rule": {"string"}}
+            "datetime"}, "text_rule": {"string", "text"}, "categorical_rule": {"string"}}
 
-        if rule_name == None or variables == None:
-            return effective_column_names
+        if not variables:
+            raise TypeError("Variables not defined")
 
         if rule_name not in rule_name_to_data_types:
-            return effective_column_names
+            # change exception to more specific one
+            raise Exception(f"'{rule_name}' is not a valid Rule name")
 
         profile_path: str = variables["parameter_nodes"]["variables"]["variables"]["profile_path"]
 
@@ -117,7 +149,12 @@ class DataProfilerColumnDomainBuilder(ColumnDomainBuilder):
 
         data_types_from_rule = rule_name_to_data_types[rule_name.lower()]
 
+        if report["data_stats"] == None:
+            return effective_column_names
+
         for col in report["data_stats"]:
+            if col["data_type"] == None:
+                continue
             if col["data_type"].lower() in data_types_from_rule:
                 effective_column_names.append(col["column_name"])
 
